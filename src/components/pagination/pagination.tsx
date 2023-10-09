ad010973561fb1be9844d333920c33c928515590
EgorPestov <egorpestov2012@yandex.ru>
2023-10-09 13:07:30 +0400

    feat: adds pagination WIP

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,40 +1,65 @@
 import { SHOWABLE_CARDS_PER_PAGE_COUNT } from '../../const';
 import { useAppSelector } from '../../hooks/use-app-selector/use-app-selector';
 import { getProducts } from '../../store/product-process/selectors';
+import { Link, useLocation } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 export const Pagination = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  let currentPage = Number(searchParams.get('page'));
+  const productsLength = useAppSelector(getProducts).length;
 
-  const products = useAppSelector(getProducts);
+  if (!currentPage) {
+    currentPage = 1;
+  }
 
-  if (products.length > SHOWABLE_CARDS_PER_PAGE_COUNT) {
+  const pagesCount = Math.ceil(productsLength / 9);
+
+
+  if (productsLength > SHOWABLE_CARDS_PER_PAGE_COUNT) {
     return (
       <div className="pagination">
         <ul className="pagination__list">
           <li className="pagination__item">
-            <a
-              className="pagination__link pagination__link--active"
-              href={1}
+            <Link
+              className={`pagination__link pagination__link--text ${currentPage <= 1 ? 'visually-hidden' : ''}`}
+              to={`${AppRoute.Root}?page=${Number(currentPage) - 1}`}
+            >
+              Назад
+            </Link>
+          </li>
+          <li className="pagination__item">
+            <Link
+              className={`pagination__link ${currentPage === 1 ? 'pagination__link--active' : ''}`}
+              to={`${AppRoute.Root}?page=1`}
             >
               1
-            </a>
+            </Link>
           </li>
           <li className="pagination__item">
-            <a className="pagination__link" href={2}>
+            <Link
+              className={`pagination__link ${currentPage === 2 ? 'pagination__link--active' : ''}`}
+              to={`${AppRoute.Root}?page=2`}
+            >
               2
-            </a>
+            </Link>
           </li>
           <li className="pagination__item">
-            <a className="pagination__link" href={3}>
+            <Link
+              className={`pagination__link ${currentPage === 3 ? 'pagination__link--active' : ''}`}
+              to={`${AppRoute.Root}?page=3`}
+            >
               3
-            </a>
+            </Link>
           </li>
           <li className="pagination__item">
-            <a
-              className="pagination__link pagination__link--text"
-              href={2}
+            <Link
+              className={`pagination__link pagination__link--text ${currentPage >= pagesCount ? 'visually-hidden' : ''}`}
+              to={`${AppRoute.Root}?page=${currentPage + 1}`}
             >
               Далее
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
